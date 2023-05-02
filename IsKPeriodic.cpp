3e31703cbcd06339d5fab0adc3dda7ea4ee75659
Андрей Курбанов <kurban1111@mail.ru>
2023-05-02 14:22:16 +0300

    Add check that k > 0 and that the length of the string is a multiple of k

---
+++ IsKPeriodic.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+
+bool IsKPeriodic(const std::string& str, int k) {
+    // ѕровер€ем, что k > 0 и что длина строки кратна k.
+    if (k <= 0 || str.length() % k != 0) {
+        return false;
+    }
+
+
+
+
+
+}
+
+
+int main() {
+    
+}
