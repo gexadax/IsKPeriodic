8c18365f24f811cde3f60fcddda22d9a5fae6623
Андрей Курбанов <kurban1111@mail.ru>
2023-05-02 14:23:55 +0300

    Add main function

--- IsKPeriodic.cpp
+++ IsKPeriodic.cpp
@@ -16,5 +16,13 @@ bool IsKPeriodic(const std::string& str, int k) {
 
 
 int main() {
-    
+    std::string str = "abcabcabcabc";
+    int k = 3;
+    if (IsKPeriodic(str, k)) {
+        std::cout << str << " is " << k << "-periodic" << std::endl;
+    }
+    else {
+        std::cout << str << " is not " << k << "-periodic" << std::endl;
+    }
+    return 0;
 }
